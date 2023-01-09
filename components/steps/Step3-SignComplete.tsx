b35a8ba2879fd560c312fdea4c21b0396fb96d5c
spreadmycode <spreadmycode@outlook.com>
2023-01-09 15:02:13 -0800

    fix: error page icon size

--- components/steps/Step3-SignComplete.tsx
+++ components/steps/Step3-SignComplete.tsx
@@ -13,7 +13,7 @@ const SignComplete = (props: any) => {
           return (
             <li key={index} className="flex justify-start items-start">
               <svg
-                className="w-5 h-5 mt-[5px] mr-1.5 text-green-500 dark:text-green-400 flex-shrink-0"
+                className="w-5 h-5 mt-[3px] mr-1.5 text-green-500 dark:text-green-400 flex-shrink-0"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
