ec68b82239077c276dd226237ff331dc2e8ee68a
webbywolf <info.webbywolf@gmail.com>
2023-01-20 23:18:58 +0530

    minor changes on homepage done

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -35,27 +35,27 @@ const Banner = (props: Props) => {
         className={
           !showBanner
             ? "hidden"
-            : "flex z-50 fixed justify-between py-3 px-4 w-full bg-[#FACA15] border-b border-gray-200 dark:border-gray-700"
+            : "flex z-50 fixed justify-between py-3 px-4 w-full bg-[#FDF6B2] border-b border-gray-200 dark:border-gray-700"
         }
       >
         <div className="flex items-center mx-auto gap-2">
           <svg
-            width="15"
-            height="15"
-            viewBox="0 0 20 20"
+            width={20}
+            height={18}
+            viewBox="0 0 20 18"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              d="M10 6V10M10 14H10.01M19 10C19 11.1819 18.7672 12.3522 18.3149 13.4442C17.8626 14.5361 17.1997 15.5282 16.364 16.364C15.5282 17.1997 14.5361 17.8626 13.4442 18.3149C12.3522 18.7672 11.1819 19 10 19C8.8181 19 7.64778 18.7672 6.55585 18.3149C5.46392 17.8626 4.47177 17.1997 3.63604 16.364C2.80031 15.5282 2.13738 14.5361 1.68508 13.4442C1.23279 12.3522 1 11.1819 1 10C1 7.61305 1.94821 5.32387 3.63604 3.63604C5.32387 1.94821 7.61305 1 10 1C12.3869 1 14.6761 1.94821 16.364 3.63604C18.0518 5.32387 19 7.61305 19 10Z"
-              stroke="#FACA15"
+              d="M9.99999 6.99999V8.99999M9.99999 13H10.01M3.07199 17H16.928C18.468 17 19.43 15.333 18.66 14L11.732 1.99999C10.962 0.666994 9.03799 0.666994 8.26799 1.99999L1.33999 14C0.569985 15.333 1.53199 17 3.07199 17Z"
+              stroke="#111928"
               strokeWidth={2}
               strokeLinecap="round"
               strokeLinejoin="round"
-            ></path>
+            />
           </svg>
 
-          <p className="text-sm font-medium text-black md:my-0 ">
+          <p className="text-sm font-semibold text-black md:my-0 ">
             {/* <span className="hidden md:inline-flex bg-primary-100 text-primary-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
               New
             </span> */}
