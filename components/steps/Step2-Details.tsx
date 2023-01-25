9d8373f29e19de30f3fd30cf50df9894ac727bec
Rotimi <70343818+Lazy-poet@users.noreply.github.com>
2023-01-25 03:36:11 +0100

    change helper text of email field

--- components/steps/Step2-Details.tsx
+++ components/steps/Step2-Details.tsx
@@ -7,7 +7,7 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { useSystemValues } from "@/contexts/ValueContext";
 
 const Details = (props: any) => {
-  const { data, fdEvents, handleFormChange } = props;
+  const { data, fdEvents, handleFormChange, handleOpen } = props;
   const [Dates, setDates] = useState<string[]>([]);
   const [Months, setMonths] = useState<string[]>([]);
   const [Years, setYears] = useState<string[]>([]);
@@ -179,28 +179,28 @@ const Details = (props: any) => {
             Email Address
           </label>
           <div className="flex">
-            <span className="inline-flex flex-col justify-center items-center px-4 text-[8px] text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="18"
-                height="18"
-                className="bi bi-shield-fill-check text-[#25D0BC] mb-1 fill-green-700 dark:fill-green-600"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0zm2.146 5.146a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647z"
-                />
-              </svg>
-              SECURE
-            </span>
             <div className="icon-input w-full">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg
+                  aria-hidden="true"
+                  className={`w-5 h-5 text-gray-500 dark:text-gray-400 ${
+                    !(fdEvents.email || Utils.validateEmail(data.email)) &&
+                    "error"
+                  }`}
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+                  <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+                </svg>
+              </div>
               <input
                 type="email"
                 name="email"
                 id="email"
-                placeholder="Email Address"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-lg rounded-tr-lg rounded-br-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-500 dark:placeholder-opacity-75 dark:text-white"
+                placeholder="name@example.com"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-lg rounded-lg rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full pl-10 p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-500 dark:placeholder-opacity-75 dark:text-white"
                 required
                 maxLength={64}
                 value={data.email}
@@ -209,24 +209,27 @@ const Details = (props: any) => {
               <span className="form-icon"></span>
             </div>
           </div>
-          {fdEvents.email ? (
+          {fdEvents.email || Utils.validateEmail(data.email) ? (
             <p
               id="helper-text-explanation"
               className="mt-2 text-sm text-gray-500 dark:text-gray-400"
             >
-              We need your email to keep you updated on your claim
-            </p>
-          ) : !data.email ? (
-            <p className="mt-2 text-sm text-red-600 dark:text-red-500">
-              We need your email to keep you updated on your claim
-            </p>
-          ) : !Utils.validateEmail(data.email) ? (
-            <p className="mt-2 text-sm text-red-600 dark:text-red-500">
-              Please provide a valid Email Address
+              We’ll never share your details. Read our{" "}
+              <a
+                href="#"
+                className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleOpen("privacy-policy.pdf");
+                }}
+              >
+                Privacy Policy
+              </a>
+              .
             </p>
           ) : (
-            <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">
-              We need your email to keep you updated on your claim
+            <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+              Please provide a valid Email Address
             </p>
           )}
         </div>
