245c909fea64ac20b6ce994ef62c375dddf2a532
Rotimi <70343818+Lazy-poet@users.noreply.github.com>
2023-01-29 00:03:23 +0100

    allow autocomplete for postcode field

--- components/steps/Step2-Details.tsx
+++ components/steps/Step2-Details.tsx
@@ -271,7 +271,7 @@ const Details = (props: any) => {
               </svg>
             </div>
             <input
-              type="search"
+              type="text"
               id="postCode"
               name="postCode"
               className=" block w-full p-4 pl-10 sm:text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-500 dark:placeholder-opacity-75 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
