11358dd8f4727202f0e048e0638988adfa9b9d6d
Rotimi <70343818+Lazy-poet@users.noreply.github.com>
2023-01-28 06:07:22 +0100

    add utility functions for comparison and checking object emptiness

--- libs/utils.ts
+++ libs/utils.ts
@@ -6,7 +6,21 @@ export const validatePAYE = (e: string) => {
   return /(\w|[0-9])+\/+\w|[0-9]+i/.test(e) ? true : false;
 };
 
+const hasObjectValueChanged = (newObj: Record<string, any>, baseObj: Record<string, any>) => {
+  return Object.keys(newObj).some((key) => newObj[key] !== baseObj[key]);
+}
+
+const isObjectFilled = (obj: Record<string, any>, fields?: string[]) => {
+  return (
+    !!Object.keys(obj).length &&
+    [...(Array.isArray(fields) && fields.length ? fields : Object.keys(obj))]
+      .every(key => !["", null, undefined].includes(obj[key]))
+  )
+}
+
 export default {
-  validateEmail: validateEmail,
-  validatePAYE: validatePAYE,
+  validateEmail,
+  validatePAYE,
+  hasObjectValueChanged,
+  isObjectFilled
 };
