b5776a4acc3cebc1b66e80c228857df9900eac34
Rotimi <70343818+Lazy-poet@users.noreply.github.com>
2023-03-06 09:37:29 +0100

    fix issue with postcode

--- components/Claim.tsx
+++ components/Claim.tsx
@@ -13,7 +13,7 @@ import StepAlert from "@/components/StepAlert";
 import ClaimLayout from "@/components/Layout";
 import Utils from "../libs/utils";
 const isNino = require("is-national-insurance-number");
-import { isValid } from "postcode";
+import { isValid, parse } from "postcode";
 import supabase from "utils/client";
 import { useSystemValues } from "@/contexts/ValueContext";
 import { Worker } from "@react-pdf-viewer/core";
@@ -431,7 +431,7 @@ function Claim({ setReady, data }: ClaimProps) {
         firstName: data?.[0]?.firstName ? data?.[0].firstName : "",
         lastName: data?.[0].lastName ? data?.[0].lastName : "",
         email: data?.[0].email ? data?.[0].email : "",
-        postCode: data?.[0].postCode ? data?.[0].postCode : "",
+        postCode: data?.[0].postCode ? parse(data?.[0].postCode).postcode : "",
         address: data?.[0].address ? data?.[0].address : "",
         day: dob.day,
         month: dob.month,
