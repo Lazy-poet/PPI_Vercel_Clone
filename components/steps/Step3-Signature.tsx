c95101a291ff5e38b73740a261a99ef0d41f9deb
Rotimi <70343818+Lazy-poet@users.noreply.github.com>
2023-04-01 11:46:33 +0100

    amend signature page

--- components/steps/Step3-Signature.tsx
+++ components/steps/Step3-Signature.tsx
@@ -65,28 +65,6 @@ const Signature = (props: any) => {
 
   return (
     <div className="mt-6">
-      <ul className="space-y-1 list-inside text-gray-500 dark:text-gray-400">
-        {CONFIRMS.map((confirm, index) => {
-          return (
-            <li key={index} className="flex justify-start items-start">
-              <svg
-                className="w-5 h-5 mt-[3px] mr-1.5 text-green-500 dark:text-green-400 flex-shrink-0"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              {confirm}
-            </li>
-          );
-        })}
-      </ul>
-
       <div
         className={`form-group mt-6 ${
           data.firstEvent ? "" : data.signatureData ? "success" : "error"
@@ -96,7 +74,7 @@ const Signature = (props: any) => {
           htmlFor="first-name"
           className="block mb-2 text-lg font-bold text-gray-900 dark:text-white"
         >
-          Draw your signature
+          Sign in the box below
         </label>
 
         <div className="w-full border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
