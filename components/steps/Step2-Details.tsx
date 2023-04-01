f715cdb5fb91bc977da73237470252cdc0243899
Rotimi <70343818+Lazy-poet@users.noreply.github.com>
2023-04-01 14:22:49 +0100

    accept a minimum of 2 chars in name fields

--- components/steps/Step2-Details.tsx
+++ components/steps/Step2-Details.tsx
@@ -133,7 +133,7 @@ const Details = (props: any) => {
       <div className="grid gap-5 mt-6 mb-5 sm:grid-cols-2">
         <div
           className={`form-group ${
-            fdEvents.firstName ? "" : data.firstName ? "success" : "error"
+            fdEvents.firstName ? "" : data.firstName.length > 1 ? "success" : "error"
           }`}
         >
           <label
@@ -156,17 +156,19 @@ const Details = (props: any) => {
             />
             <span className="form-icon"></span>
           </div>
-          {fdEvents.firstName
-            ? ""
-            : !data.firstName && (
-                <p className="mt-2 text-sm">
-                  Please let us know your first name
-                </p>
-              )}
+          {fdEvents.firstName ? (
+            ""
+          ) : !data.firstName ? (
+            <p className="mt-2 text-sm">Please let us know your first name</p>
+          ) : (
+            data.firstName.length === 1 && (
+              <p className="mt-2 text-sm">Please enter a valid name</p>
+            )
+          )}
         </div>
         <div
           className={`form-group ${
-            fdEvents.lastName ? "" : data.lastName ? "success" : "error"
+            fdEvents.lastName ? "" : data.lastName.length > 1 ? "success" : "error"
           }`}
         >
           <label
@@ -189,13 +191,19 @@ const Details = (props: any) => {
             />
             <span className="form-icon"></span>
           </div>
-          {fdEvents.lastName
-            ? ""
-            : !data.lastName && (
-                <p className="mt-2 text-sm text-red-600 dark:text-red-500">
-                  Please let us know your last name
-                </p>
-              )}
+          {fdEvents.lastName ? (
+            ""
+          ) : !data.lastName ? (
+            <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+              Please let us know your last name
+            </p>
+          ) : (
+            data.lastName.length === 1 && (
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                Please enter a valid name
+              </p>
+            )
+          )}
         </div>
         <div
           className={`form-group sm:col-span-2 ${
