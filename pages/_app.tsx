9cb2c44809707dfa0fec990e9496a3a0aefec2f7
Rotimi <70343818+Lazy-poet@users.noreply.github.com>
2023-04-05 12:30:32 +0100

    implement polyfill for Array.at to support older browsers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
+import "../libs/polyfill";
 import type { AppProps } from "next/app";
 import { ValueProvider } from "@/contexts/ValueContext";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import BugsnagProvider from "@/components/BugsnagProvider";
 import { useEffect } from "react";
 import getConfig from "next/config";
+
 export default function App({ Component, pageProps }: AppProps) {
   const { publicRuntimeConfig } = getConfig();
 
