44a375d716fd9b0485c3abbd93f1061d4880f065
Rotimi <70343818+Lazy-poet@users.noreply.github.com>
2023-04-09 23:28:44 +0100

    use default button for postcode button

--- components/steps/Step2-Details.tsx
+++ components/steps/Step2-Details.tsx
@@ -349,7 +349,7 @@ const Details = (props: any) => {
               type="button"
               className={`${
                 showPulse ? "search-pulse" : ""
-              } text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg sm:text-lg px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
+              } absolute right-2.5 bottom-2.5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
               onClick={() => {
                 searchAddressByPostcode(data.postCode);
                 if (showPulse) {
