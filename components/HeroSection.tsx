8e8b3aaad4dbdb3bae9f525fd5ba7828757be1d9
Rotimi <70343818+Lazy-poet@users.noreply.github.com>
2023-04-25 14:07:39 +0100

    update buton colors

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -411,7 +411,7 @@ const HeroSection: React.FC<{
                 <li className="md:col-span-2">
                   <div className="text-center">
                     <button
-                      className="inline-flex justify-between items-center p-5 w-full focus:outline-none text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-lg dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                      className="inline-flex justify-between items-center p-5 w-full focus:outline-none text-white bg-[#00A400] hover:bg-[#00A100] focus:ring-4 focus:ring-green-300 font-medium rounded-lg dark:bg-[#00A400] dark:hover:bg-[#00A100] dark:focus:ring-green-800"
                       onClick={handleClick}
                     >
                       <div />
