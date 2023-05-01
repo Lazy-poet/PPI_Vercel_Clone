d1cb052e53ab7931ab3d9168d9cf6c1d55eb822c
Rotimi <70343818+Lazy-poet@users.noreply.github.com>
2023-05-01 22:09:22 +0100

    reword ssl helper text

--- components/SslSecure.tsx
+++ components/SslSecure.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import SslImg from "../public/images/ssl-secure.svg";
+import InputHelper from "./InputHelper";
 
 type Props = {};
 
 const SslSecure = (props: Props) => {
   return (
     <>
-      <p className="text-sm text-gray-500 dark:text-gray-400 mt-2 text-center sm:text-left mx-auto w-fit sm:w-full sm:mx-0">
-        <span>This form is securely encrypted to ensure your data is safe</span>
-      </p>
+      <InputHelper
+        text="This form is encrypted to ensure your data is safe"
+        align="center"
+      />
       <Image
         className="w-28 md:w-32 mt-5 mx-auto sm:mx-0"
         src={SslImg}
