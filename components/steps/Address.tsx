17ad06bd259eb00e5f9bb34b863d1ec64ccd8682
Rotimi <70343818+Lazy-poet@users.noreply.github.com>
2023-05-11 09:59:00 +0100

    revert address blockquote

--- components/steps/Address.tsx
+++ components/steps/Address.tsx
@@ -245,7 +245,7 @@ const Address = () => {
           </div>
         ) : null}
         {selectedAddress && (
-          <blockquote className=" w-full p-4 border-l-4 border-gray-300 dark:border-gray-500">
+          <blockquote className="sm:col-span-2 p-4 border-l-4 border-gray-300 bg-gray-50 dark:border-gray-500 dark:bg-gray-800">
             {selectedAddress.split(",").map((address, i) => (
               <p
                 key={i}
